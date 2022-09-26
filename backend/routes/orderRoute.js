d8c6ffdddc479cc994d6770e6e3430bb4e9fdd1c
rjhelm <rjhelm@github.io>
2022-09-26 16:54:09 -0600

    created controllers for order, product, payment, and user:

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const { newOrder, getSingleOrderDetails, myOrders, getAllOrders, updateORder, deleteOrder } = require('../controllers/orderControllers');
-const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
+const { newOrder, getSingleOrderDetails, myOrders, getAllOrders, updateOrder, deleteOrder } = require('../controllers/orderController');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.route('/order/new').post(isAuthenticatedUser, newOrder);
 router.route('/order/:id').get(isAuthenticatedUser, getSingleOrderDetails);
-router.route
+router.route('/orders/me').get(isAuthenticatedUser, myOrders);
+
+router.route('/admin/orders').get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+
+router.route('/admin/order/:id')
+    .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+    .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
+
+module.exports = router;
