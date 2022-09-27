622ffd58ddecd8443e94a5278ef0af59e58388aa
rjhelm <rjhelm@github.io>
2022-09-26 21:07:42 -0600

    added some basic dependencies || cleaned up file structure || added helpful utils that will be used throughout app

---
+++ frontend/src/utils/states.js
@@ -0,0 +1,53 @@
+const states = [
+    {
+        "code": "UT",
+        "name": "Utah"
+    },
+    {
+        "code": "AZ",
+        "name": "Arizona",
+    },
+    {
+        "code": "CA",
+        "name": "California",
+    },
+    {
+        "code": "ID",
+        "name": "Idaho",
+    },
+    {
+        "code": "NV",
+        "name": "Nevada",
+    },
+    {
+        "code": "WA",
+        "name": "Washington",
+    },
+    {
+        "code": "CO",
+        "name": "Colirado",
+    },
+    {
+        "code": "MO",
+        "name": "Montana",
+    },
+    {
+        "code": "OR",
+        "name": "Oregon",
+    },
+    {
+        "code": "NM",
+        "name": "New Mexico",
+    },
+    {
+        "code": "TX",
+        "name": "Texas",
+    },
+    {
+        "code": "OK",
+        "name": "Oklahoma",
+    },
+    
+]
+
+export default states;
